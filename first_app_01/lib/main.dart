a46207192d8ac0e109c0f17c300f1d951f932cf6
myrows <milenio204@hotmail.com>
2020-04-07 13:55:56 +0200

    [FIX] issues with run

--- first_app_01/lib/main.dart
+++ first_app_01/lib/main.dart
@@ -92,7 +92,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'Taps :',
+              'Tapss :',
             ),
             Text(
               '$_counter',
